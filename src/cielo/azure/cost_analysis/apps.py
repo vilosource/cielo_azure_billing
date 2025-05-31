6291677398a5487baffd12692c72144e2861511f
JV <126692+vilosource@users.noreply.github.com>
2025-05-31 17:38:38 +0300

    fix: set correct app config name

--- src/cielo/azure/cost_analysis/apps.py
+++ src/cielo/azure/cost_analysis/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
 
-class BillingConfig(AppConfig):
+class CostAnalysisConfig(AppConfig):
+    """Application configuration for the cost analysis app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'billing'
+    name = 'cielo.azure.cost_analysis'
 
     def ready(self):
         from . import signals  # Import signals to register them
