39d4954835bf7e5235d16f75895611cb12420e66
JV <126692+vilosource@users.noreply.github.com>
2025-05-31 17:04:28 +0300

    Fix project module references

--- example_django_project/asgi.py
+++ example_django_project/asgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cielo_azure_billing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example_django_project.settings')
 application = get_asgi_application()
--- example_django_project/wsgi.py
+++ example_django_project/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cielo_azure_billing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example_django_project.settings')
 application = get_wsgi_application()
